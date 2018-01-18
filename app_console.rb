6fd03f09c9377554f59fdf7fb32fe5feea619f00
sherry0738 <33485908+sherry0738@users.noreply.github.com>
2018-01-18 21:50:06 +1100

    Update app_console.rb

--- app_console.rb
+++ app_console.rb
@@ -1,1 +1,9 @@
+require 'pry'
+require_relative 'db_config'
+require_relative 'models/photographer'
+require_relative 'models/comment'
+require_relative 'models/user'
 
+binding.pry
+
+puts 'app_console'
