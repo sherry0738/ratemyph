49c2a48a4dae2d725f4c0d50927e717555bd4009
sherry0738 <33485908+sherry0738@users.noreply.github.com>
2018-01-18 22:24:43 +1100

    Update db_config.rb

--- db_config.rb
+++ db_config.rb
@@ -3,7 +3,11 @@ require 'active_record'
 
 options = {
 	adapter: 'postgresql',
-	database: 'ratemyphotographer',
+	database: 'dejtrt7qqtlea5',
+	host: 'ec2-54-197-233-123.compute-1.amazonaws.com',
+	port: 5432,  
+	username:'qrgcvidzgleqjy',   
+	password: 'ea8cd2efbdd1f98c9b5c070fb41e3c3f64ca7ce8ec562ff8db72e49b007af43a', 
 }
 
 ActiveRecord::Base.establish_connection(options)
